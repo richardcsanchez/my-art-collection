602b66a44024508763586083a82213f1fc115aa5
Richard Sanchez <richard.charles.s@gmail.com>
2018-11-07 22:31:36 +0000

    message for first commit

---
+++ lib/my/art/catalogue.rb
@@ -0,0 +1,9 @@
+require "my/art/catalogue/version"
+
+module My
+  module Art
+    module Catalogue
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/my/art/catalogue/version.rb
@@ -0,0 +1,7 @@
+module My
+  module Art
+    module Catalogue
+      VERSION = "0.1.0"
+    end
+  end
+end
