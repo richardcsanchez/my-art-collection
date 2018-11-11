63685d3ec401b6cc5015bd3fb4b1687f7ef54bf4
Richard Sanchez <richard.charles.s@gmail.com>
2018-11-11 22:43:12 +0000

    update create_artwork form and contollers"

--- app/controllers/artwork_controller.rb
+++ app/controllers/artwork_controller.rb
@@ -1,12 +1,23 @@
 class ArtworkController <ApplicationController
 
   get '/artworks' do
-    erb :artworks
+    erb :'artworks/artworks'
   end
-  
+
   get '/artworks/new'do
     erb :'artworks/create_artwork'
   end
 
-  post '/artworks'
+  post '/artworks' do
+    @artwork = Artwork.new(params[:artwork])
+    if !params["artist"]["name"].empty?
+      binding.pry
+      @artwork.artists << Artist.new(params[:artist])
+    end
+    @artwork.save
+
+    redirect to "/artworks"
+  end
+
+
 end
