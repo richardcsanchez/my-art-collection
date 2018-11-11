a9003693f0c0bc9ce2b841ed8825692366f8ecf4
Richard Sanchez <richard.charles.s@gmail.com>
2018-11-11 22:00:29 +0000

    update artwork controller and artwork.erb

--- app/controllers/artwork_controller.rb
+++ app/controllers/artwork_controller.rb
@@ -1,6 +1,12 @@
 class ArtworkController <ApplicationController
 
+  get '/artworks' do
+    erb :artworks
+  end
+  
   get '/artworks/new'do
     erb :'artworks/create_artwork'
   end
+
+  post '/artworks'
 end
