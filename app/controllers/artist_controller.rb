65fb3690f5a2c1c1fa1f47f21f98081d749bd51b
Richard Sanchez <richard.charles.s@gmail.com>
2018-11-13 20:53:09 +0000

    add artists/:id route, begin building show_artist view

--- app/controllers/artist_controller.rb
+++ app/controllers/artist_controller.rb
@@ -1,2 +1,12 @@
 class ArtistController <ApplicationController
+
+get '/artists' do
+  erb :'artists/artists'
+end
+
+get '/artists/:id' do
+  @artist = Artist.find_by_id(params[:id])
+  erb :'artists/show_artist'
+end
+
 end
