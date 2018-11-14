4eaca7131fe876f9bf7f34a21e61f56ee3e2d94e
Richard Sanchez <richard.charles.s@gmail.com>
2018-11-14 00:51:54 +0000

    add delete artist route

--- app/controllers/artist_controller.rb
+++ app/controllers/artist_controller.rb
@@ -9,4 +9,21 @@ get '/artists/:id' do
   erb :'artists/show_artist'
 end
 
+get '/artists/:id/delete' do
+  binding.pry
+
+  if Helpers.is_logged_in?(session)
+    @artist = Artist.find_by_id(params[:id])
+  else
+    redirect to '/login'
+  end
+
+  if  @artist.artworks == nil
+    @artist.delete
+      redirect to '/artworks'
+  else
+    redirect to "/artists"
+  end
+end
+
 end
